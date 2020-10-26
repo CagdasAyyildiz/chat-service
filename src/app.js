2064e0e4a9432aa6721368c99d3a3c60b635e9f3
CagdasAyyildiz <cagdasayyildiz.ayyildiz@gmail.com>
2020-10-26 13:15:54 +0300

    "Add cors middleware"

--- src/app.js
+++ src/app.js
@@ -5,9 +5,13 @@ const Room = require('./models/Room')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const cors = require('cors')
+
 
 dotenv.config();
-app.use(morgan('[:date[web]] || :method :url  || Status: :status || Response time: :response-time ms'))
+app.use(morgan('[:date[web]] || :method :url  || Status: :status || Response time: :response-time ms'));
+app.use(cors())
+
 const MONGOOSE_OPTIONS = {useNewUrlParser: true, useUnifiedTopology: true};
 mongoose.connect(process.env.DB_CONNECTION, MONGOOSE_OPTIONS, () => {
     console.log('Connected to MongoDB database');
@@ -39,31 +43,6 @@ io.on('connection', (socket) => {
 
 
 });
-/*
-const initializeRoom = async (users) => {
-    let oldMessages = await Room.findOne({users});
-    console.log(users);
-    if (!oldMessages) {
-        const room = new Room({
-            users,
-            messages: [],
-        });
-        await room.save();
-        io.emit('load messages', oldMessages);
-        socket.on('new message', newMessage);
-    }
-}
-
-const newMessage = async (data) => {
-    console.log(data);
-    const room = await Room.findOne({users: users});
-    room.messages = [...room.messages, {content: data.message, sentBy: data.sentBy}];
-    console.log(room);
-    //await room.save();
-    let allMessages = await Room.findOne({users});
-    io.emit('load messages', allMessages);
-}
-*/
 const PORT = process.env.PORT || '3000';
 http.listen(PORT, () => {
     console.log('listening on *:3000');
