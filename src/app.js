8c4f732638b01ece7c2cd25cf600dc6e2bc74a21
CagdasAyyildiz <cagdasayyildiz.ayyildiz@gmail.com>
2020-10-26 13:27:53 +0300

    "Add cors middleware"

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 const Room = require('./models/Room')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
+const io = require('socket.io')(http, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 
 
 dotenv.config();
