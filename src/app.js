d905d919932f98a55362832954b959b18aaa55df
CagdasAyyildiz <cagdasayyildiz.ayyildiz@gmail.com>
2020-10-26 13:39:16 +0300

    "Add cors middleware"

--- src/app.js
+++ src/app.js
@@ -5,47 +5,49 @@ const Room = require('./models/Room')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const cors = require('cors')
 
 
 dotenv.config();
 app.use(morgan('[:date[web]] || :method :url  || Status: :status || Response time: :response-time ms'));
-app.use(cors())
 
 const MONGOOSE_OPTIONS = {useNewUrlParser: true, useUnifiedTopology: true};
 mongoose.connect(process.env.DB_CONNECTION, MONGOOSE_OPTIONS, () => {
     console.log('Connected to MongoDB database');
 });
 
-io.set('origins', '*') // for latest version
-app.get('/socket.io/', cors(), () => {
-    io.on('connection', (socket) => {
-        socket.on('initialize', async (users) => {
-            let oldMessages = await Room.findOne({users});
-            console.log(users);
-            if (!oldMessages) {
-                const room = new Room({
-                    users,
-                    messages: [],
-                });
-                await room.save();
-            }
-            io.emit('load messages', oldMessages);
-            socket.on('new message', async (data) => {
-                console.log(data);
-                const room = await Room.findOne({users: users});
-                room.messages = [...room.messages, {content: data.message, sentBy: data.sentBy}];
-                console.log(room);
-                //await room.save();
-                let allMessages = await Room.findOne({users});
-                io.emit('load messages', allMessages);
-            });
+io.origins((origin, callback) => {
+    if (origin !== '*') {
+        return callback('origin not allowed', false);
+    }
+    callback(null, true);
+});
 
+io.on('connection', (socket) => {
+    socket.on('initialize', async (users) => {
+        let oldMessages = await Room.findOne({users});
+        console.log(users);
+        if (!oldMessages) {
+            const room = new Room({
+                users,
+                messages: [],
+            });
+            await room.save();
+        }
+        io.emit('load messages', oldMessages);
+        socket.on('new message', async (data) => {
+            console.log(data);
+            const room = await Room.findOne({users: users});
+            room.messages = [...room.messages, {content: data.message, sentBy: data.sentBy}];
+            console.log(room);
+            //await room.save();
+            let allMessages = await Room.findOne({users});
+            io.emit('load messages', allMessages);
         });
 
-
     });
-})
+
+
+});
 
 const PORT = process.env.PORT || '3000';
 http.listen(PORT, () => {
